de3cc8c34e4a3b35375cbe47b386e248b780962a
niknik19 <1nik-nik@mail.ru>
2015-01-25 21:42:59 +0300

    Added creating mine field after first click

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -22,8 +22,8 @@
         
         var gameSelf = this;  
         var defaultOptions = {
-            vCellCount : 40, // vertical cells count
-            hCellCount : 40, // horizantal cells count
+            vCellCount : 30, // vertical cells count
+            hCellCount : 30, // horizantal cells count
             /*
                 0 1 2 3 - > Horizantal
                 1 * * * 
@@ -40,8 +40,9 @@
                 
         this.vCellCount = options.vCellCount || defaultOptions.vCellCount;
         this.hCellCount = options.hCellCount || defaultOptions.hCellCount;
-        this.minesCount = options.minesCount || options.minesCount;
+        this.minesCount = options.minesCount || defaultOptions.minesCount;
         this.hostGameElement = document.getElementById(hostGameElementId);
+        this.isGameboardInitialized = false;
         
         this.gameBoard = new Array(this.hCellCount);
         for (var i = 0; i < this.hCellCount; i++) {
@@ -65,14 +66,53 @@
                 }
                 
                 return isRightMB;
+            },
+            
+            cellClick: function(event) {
+                var cellCoord = { 
+                    cellX : this.cellX,
+                    cellY : this.cellY
+                };
+                
+                if (gameSelf.utils.isRightMB(event)) {
+                    gameSelf.rightClick(cellCoord);
+                } else { 
+                    gameSelf.leftClick(cellCoord);
+                }
             }
         };
         
         this.rightClick = function(cellCoord) {
+            gameSelf.revealCell(cellCoord);
         };
         
+        this.initGameboard = function(choosenCellCoord) {
+            var minesCreated = 0; 
+            
+            while (minesCreated < gameSelf.minesCount) {
+                var proposedX = Math.floor(Math.random() * gameSelf.hCellCount);
+                var proposedY = Math.floor(Math.random() * gameSelf.vCellCount);
+                
+                if (gameSelf.gameBoard[proposedX, proposedY].status !== CELL_STATUS.HIDDEN_MINE || 
+                   ( choosenCellCoord.cellX !== proposedX && choosenCellCoord.cellY !== proposedY)){
+                    gameSelf.status = CELL_STATUS.HIDDEN_MINE;
+                    minesCreated += 1;
+                    
+                    gameSelf.gameBoard[proposedX][proposedY].status = CELL_STATUS.HIDDEN_MINE;
+                    gameSelf.gameBoard[proposedX][proposedY].element.className = "cell mine-hidden";
+                }
+            }
+            
+            gameSelf.isGameboardInitialized = true;
+        }
+        
         this.leftClick = function(cellCoord) {           
-            gameSelf.revealCell(cellCoord);
+            //gameSelf.revealCell(cellCoord);
+            if (!gameSelf.isGameboardInitialized) {
+                gameSelf.initGameboard();
+            }
+            
+            
             
             if(gameSelf.isWin()) {
                 gameSelf.showWin();
@@ -83,7 +123,7 @@
         };
         
         this.revealCell = function(cellCoord) {
-            alert ("X: " + cellCoord.X + ", Y: " + cellCoord.Y);
+            alert ("X: " + cellCoord.cellX + ", Y: " + cellCoord.cellY);
         };
         
         this.isWin = function() { 
@@ -104,39 +144,45 @@
             
         };
             
-        this.getCellCoord = function(event) {
-            var totalOffsetX = 0;
-            var totalOffsetY = 0;
-            var elementX = 0;
-            var elementY = 0;
-            var currentElement = this;
-
-            do {
-                totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
-                totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
-            }
-            while(currentElement = currentElement.offsetParent)
-
-            elementX = event.pageX - totalOffsetX;
-            elementY = event.pageY - totalOffsetY;
-
-            return { 
-                X: Math.floor( elementX / gameSelf.cellWidth), 
-                Y: Math.floor( elementY / gameSelf.cellHeight)
-            };
-        };
+//       this.getCellCoord = function(event) {
+//            var totalOffsetX = 0;
+//            var totalOffsetY = 0;
+//            var elementX = 0;
+//            var elementY = 0;
+//            var currentElement = this;
+//
+//            do {
+//                totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+//                totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+//            }
+//            while(currentElement = currentElement.offsetParent)
+//
+//            elementX = event.pageX - totalOffsetX;
+//            elementY = event.pageY - totalOffsetY;
+//
+//            return { 
+//                X: Math.floor( elementX / gameSelf.cellWidth), 
+//                Y: Math.floor( elementY / gameSelf.cellHeight)
+//            };
+//        };
+         
+//           return { 
+//               X: Math.floor( event.pageX / gameSelf.cellWidth),
+//               Y: Math.floor( event.pageY / gameSelf.cellHeight)
+//           };
+//       };
         
         this.init = function () {  
             
-            gameSelf.hostGameElement.addEventListener("click", function(event) {
-                var cellCoord = gameSelf.getCellCoord.call(this, event);
-                
-                if (gameSelf.utils.isRightMB(event)) {
-                    gameSelf.rightClick(cellCoord);
-                } else { 
-                    gameSelf.leftClick(cellCoord);
-                }
-            });
+//            gameSelf.hostGameElement.addEventListener("click", function(event) {
+//                var cellCoord = gameSelf.getCellCoord.call(this, event);
+//                
+//                if (gameSelf.utils.isRightMB(event)) {
+//                    gameSelf.rightClick(cellCoord);
+//                } else { 
+//                    gameSelf.leftClick(cellCoord);
+//                }
+//            });
             
             for (var i = 0; i < gameSelf.hCellCount; i++){
                 for (var j = 0; j < gameSelf.vCellCount; j++) {
@@ -147,8 +193,10 @@
                     divCell.style.height = gameSelf.cellHeight + "px";
                     divCell.style.left = gameSelf.cellWidth * i + "px";
                     divCell.style.top = gameSelf.cellHeight * j + "px";
+                    divCell.cellX = i;
+                    divCell.cellY = j;
+                    divCell.addEventListener("click", gameSelf.utils.cellClick);
 
-                    
                     gameSelf.gameBoard[i][j] = { 
                         element: divCell, 
                         status: CELL_STATUS.HIDDEN_EMPTY
